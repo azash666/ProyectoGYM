163ebc914ad15ac26a33ad8a125ee3fcb8803621
JuanDiegoMartinez <43170453+JuanDiegoMartinez@users.noreply.github.com>
2018-11-20 12:30:37 +0100

    he hecho las bolas

---
+++ Assets/Scripts/Objetos/InmuneScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InmuneScript : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Destruye la esfera si toca con el ambiente
+        if (other.tag == "Enviorament")
+        {
+            Destroy(this.gameObject);
+        }
+
+        //Si es el player se vuelve inmune
+        if (other.tag == "Player")
+        {
+            //Player es el script que ejecutará el player y pondrá el inmune a true
+            //other.GetComponentInChildren<Player>().inmuneEnabled = true;
+
+            Destroy(this.gameObject);
+        }
+    }
+}
