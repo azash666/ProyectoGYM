e448deee088ccb510778e444361f3043ed48cda3
azash666 <ddelgadogomis@hotmail.com>
2019-01-13 16:26:51 +0100

    Victory

--- Assets/Scripts/Juego/Objetivo.cs
+++ Assets/Scripts/Juego/Objetivo.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Objetivo : MonoBehaviour
 {
     public GameObject siguiente;
     public bool final;
+    public RawImage imagenVctoria;
+    public Text texto;
     // Use this for initialization
+
+    private void Start()
+    {
+        imagenVctoria.enabled = false;
+        texto.enabled = false;
+    }
+
     void OnTriggerEnter(Collider other)
     {
         
@@ -15,9 +26,25 @@ public class Objetivo : MonoBehaviour
         {
             if (final)
             {
-                Debug.Log("Fin");
+                Victory();
             }else
                 other.GetComponent<rotadorSenal>().objetivo = siguiente;
         }
+    }
+
+    void Victory()
+    {
+        // Set the death flag so this function won't be called again.
+        imagenVctoria.enabled = true;
+        texto.enabled = true;
+
+        // Reproducimos el sonido de la muerte
+        StartCoroutine(LoadLevelAfterDelay(5));
+    }
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene("menu");
     }
 }
--- Assets/Scripts/Juego/rotadorSenal.cs
+++ Assets/Scripts/Juego/rotadorSenal.cs
@@ -7,14 +7,13 @@ public class rotadorSenal : MonoBehaviour {
     public GameObject objetivo;
     public GameObject senal;
 
-	// Use this for initialization
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    // Update is called once per frame
+    void Update () {
         Transform target = objetivo.transform;
         Vector3 posicion = new Vector3(target.position.x, transform.position.y, target.position.z);
         Vector3 targetDir = target.position - transform.position;
-        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, 0.5f, 0.5f);
+        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, 2*3.14159f, 0.5f);
         senal.transform.rotation = Quaternion.LookRotation(newDir);
     }
 }
